0ef0c2e1b4c3e9eb97081e78cde6c70e49e69d29
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 17:10:45 -0600

    feat: agregue la seccion tech

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
 import { Projects } from "./components/projects/projects";
+import { TechStack } from "./components/tech-stack/tech-stack";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Navbar, Hero, Projects]
+  imports: [Navbar, Hero, Projects, TechStack]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
