ae4616cd6ffaf3a64ab7c305129f9194cdb71eff
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 12:43:10 -0600

    feat: agregue el componente navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Navbar]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
