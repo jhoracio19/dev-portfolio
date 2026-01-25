3f36555f3ed029842b67817e465ff8879570ee7d
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 17:36:37 -0600

    feat: agregue la seccion de certifiaciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,14 @@ import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
 import { Projects } from "./components/projects/projects";
 import { TechStack } from "./components/tech-stack/tech-stack";
+import { About } from "./components/about/about";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Navbar, Hero, Projects, TechStack]
+  imports: [Navbar, Hero, Projects, TechStack, About]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
