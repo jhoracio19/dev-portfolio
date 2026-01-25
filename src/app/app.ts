8ea0d10200db1fe43040141e94bbe625ad93deb3
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 17:55:49 -0600

    feat: agregue el footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,15 @@ import { Hero } from "./components/hero/hero";
 import { Projects } from "./components/projects/projects";
 import { TechStack } from "./components/tech-stack/tech-stack";
 import { About } from "./components/about/about";
+import { Experience } from "./components/experience/experience";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Navbar, Hero, Projects, TechStack, About]
+  imports: [Navbar, Hero, Projects, TechStack, About, Experience, Footer]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
