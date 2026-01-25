7f4c47f94630b1905cd05443e98ec753f92ff616
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 16:34:38 -0600

    feat: agregue la seccion de proyectos

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
+import { Projects } from "./components/projects/projects";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Navbar, Hero]
+  imports: [Navbar, Hero, Projects]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
