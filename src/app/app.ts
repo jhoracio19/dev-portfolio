f2647e50a6e7d15cba3b3acd8351274cb0706143
Jose Horacio <jhoracio19@hotmail.com>
2026-01-25 13:30:28 -0600

    feat: agregue la seccion del hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Navbar]
+  imports: [Navbar, Hero]
 })
 export class App {
   protected readonly title = signal('mi-portfolio-dev');
